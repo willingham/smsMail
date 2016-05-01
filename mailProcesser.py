1b407d2cdaf6494edb6ee37cf3e67f7b5f966000
“Thomas Willingham“ <thomas@tshows.us>
2016-05-01 18:32:44 +0400

    first commit

---
+++ mailProcesser.py
@@ -0,0 +1,19 @@
+class mailProcesser(object):
+    def __init__(self, messageContent):
+        self.rawMime = messageContent
+
+    def getSender(self):
+        continue
+
+    def getRecipient(self):
+        continue
+    
+    def getSubject(self):
+        continue
+
+    def getBody(self):
+        continue
+    
+    def checkFor(self, s):
+        continue
+
